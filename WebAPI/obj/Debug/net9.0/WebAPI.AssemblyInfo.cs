e9864b9c4047edbd5c0a877ea21f711f9122cd1b
David J. Straley <djstraley@sandyhook.com>
2025-05-14 21:55:51 -0500

    I added all of these things so as to get Swagger working but never succeeded, so I rolled back the code for adding Swagger  - the app does work directly, without Swagger, fortunately

--- WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net9.0/WebAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cc9bce04f13c66b85ab0c5922ed6bf7bf043365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a6b48da6cbbae31f23ddd9c5296b1773d632bbe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
