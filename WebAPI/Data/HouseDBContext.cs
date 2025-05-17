09ffe74a1570dc4b299c33cdf8c469351e86c8a5
David J. Straley <djstraley@sandyhook.com>
2025-05-16 23:00:22 -0500

    setting up SQL Lite and Entity Framework

---
+++ WebAPI/Data/HouseDBContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+public class HouseDBContext : DbContext
+{
+    public HouseDBContext(DbContextOptions<HouseDBContext> options) : base(options)
+    {
+    }
+
+    public DbSet<HouseEntity> Houses => Set<HouseEntity>();
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var folder = Environment.SpecialFolder.LocalApplicationData;
+        var path = Environment.GetFolderPath(folder);
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlite($"Data Source={Path.Join(path, "HouseDB.db")}");
+        }
+    }
+}
---
+++ WebAPI/Data/HouseEntity.cs
@@ -0,0 +1,11 @@
+public class HouseEntity
+{
+    public int ID { get; set; }
+    public string? Address { get; set; }
+    public string? Country { get; set; }
+    public string? Description { get; set; }
+    public decimal Price { get; set; }
+    public string? Photo { get; set; }
+
+
+} 
